e50a2451943f898c589e66ce59247c5c0b3eaeb0
Tranq <68222554@qq.com>
2020-10-12 17:35:51 +0800

    Update preload.js

--- preload.js
+++ preload.js
@@ -8,5 +8,18 @@ window.document.addEventListener('ready', () => {
 
 });
 
+dataSetRootPath = null;
+
+const { ipcRenderer } = require('electron')
+
+window.addEventListener('DOMContentLoaded', () => {
+    ipcRenderer.on('open-dataset', (e, ...args) => {
+        onOpenDataset(args[0] || { path: '', files: [] });
+    })
+});
+
+//enable ipcRenderer feature
+window.ipcRenderer = ipcRenderer;
+
 
 
